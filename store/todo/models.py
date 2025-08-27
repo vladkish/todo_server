531c644590170235d8693610154447b544c860f0
vladkish <vladkish@users.noreply.github.com>
2025-08-28 00:54:39 +0300

    Wiriting comment models Category, Task

--- store/todo/models.py
+++ store/todo/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+# class Category(models.Model):
+#     title = models.CharField(max_length=60)
+    
+#     def __str__(self):
+#         return f'{self.title}'
+
+# class Task(models.Model):
+#     title = models.CharField(max_length=120)
+#     description = models.TextField()
+#     category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name='tasks')
+    
+#     def __str__(self):
+#         return f'{self.title}, {self.description}, {self.category}'
