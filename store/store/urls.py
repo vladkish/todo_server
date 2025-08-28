353f73b3ff67f903be28d715a81c0bd2695cc89b
vladkish <vladkish@users.noreply.github.com>
2025-08-28 10:11:18 +0300

    Setting media and fix bug with AUTH_USER_MODEL

--- store/store/urls.py
+++ store/store/urls.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from todo import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('todo/', include('todo.urls', namespace='todo'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
