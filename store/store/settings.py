9c6777e0fa219ffd26e56396f1de16f9a6d300a7
vladkish <vladkish@users.noreply.github.com>
2025-08-28 10:16:58 +0300

    Createad models Task and Category

--- store/store/settings.py
+++ store/store/settings.py
@@ -94,10 +94,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -107,9 +103,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
@@ -118,8 +111,6 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- store/todo/models.py
+++ store/todo/models.py
@@ -1,15 +1,19 @@
 from django.db import models
+from users.models import User
 
-# class Category(models.Model):
-#     title = models.CharField(max_length=60)
+class Category(models.Model):
+    title = models.CharField(max_length=60)
     
-#     def __str__(self):
-#         return f'{self.title}'
+    def __str__(self):
+        return f'{self.title}'
 
-# class Task(models.Model):
-#     title = models.CharField(max_length=120)
-#     description = models.TextField()
-#     category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name='tasks')
+class Task(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    status = models.BooleanField(default=False)
+    date_public = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name='tasks')
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='tasks')
     
-#     def __str__(self):
-#         return f'{self.title}, {self.description}, {self.category}'
+    def __str__(self):
+        return f'{self.title}, {self.description}, {self.category}'
