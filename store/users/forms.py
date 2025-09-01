34e2c746e92418704ca6b48ac1e046ef45427309
vladkish <vladkish@users.noreply.github.com>
2025-09-01 20:08:50 +0300

    Writing logic login and sign

--- store/users/forms.py
+++ store/users/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from users import User
+from users.models import User
 
 class Login(AuthenticationForm):
     pass
 
 class Sign(UserCreationForm):
+
+    email = forms.EmailField(required=True)
+    
     class Meta:
         model = User
-        fields = ('email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
--- store/users/views.py
+++ store/users/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from .forms import Login, Sign
 
 def login(request):
-    return render(request, 'users/login.html')
+    
+    if request.method == "POST":
+        form = Login(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = auth.authenticate(request, username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect('index')
+    else:
+        form = Login()
+        
+    context = {
+        'form' : form
+    }
+    
+    return render(request, 'users/login.html', context)
 
 def sign(requests):
-    return render(requests, 'users/sign.html')
+    
+    if requests.method == 'POST':
+        form = Sign(data=requests.POST)
+        if form.is_valid():
+            user = form.save()
+            auth.login(requests, user)
+            return redirect('index')
+    else:
+        form = Sign()
+        
+    context = {
+        'form' : form
+    }
+    
+    return render(requests, 'users/sign.html', context)
 
 @login_required
 def profile(request):
